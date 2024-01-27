3e8173e8c92bc22ca587261f0cc56f54133de910
vsafonkin <vsafonkin@gmail.com>
2024-01-27 09:01:39 +0300

    v1.1.0

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	var num int
-	for num != -1 {
-		fmt.Printf("> ")
-		_, err := fmt.Scan(&num)
-		if err != nil {
-			fmt.Println("scan error:", err)
-			continue
-		}
-		if num == -1 {
-			continue
-		}
-		fmt.Printf("%d %#[1]x %#08[1]b\n", num)
+	fmt.Printf("> ")
+	_, err := fmt.Scan(&num)
+	if err != nil {
+		fmt.Println("scan error:", err)
+		os.Exit(1)
 	}
+
+	fmt.Printf("%d %#[1]x %#08[1]b\n", num)
 }
